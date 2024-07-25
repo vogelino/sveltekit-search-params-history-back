ace77a3765f3b328e745f0b70fabc37dec1d9929
Lucas Vogel <contact@vogelino.com>
2024-07-25 16:29:05 -0400

    Add demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { queryParameters, ssp } from 'sveltekit-search-params';
+
+	const urlState = queryParameters(
+		{
+			query: ssp.string(''),
+			bool: ssp.boolean(true),
+			slider: ssp.number(50)
+		},
+		{
+			debounceHistory: 500
+		}
+	);
+</script>
+
+<div class="flex h-screen w-screen flex-col items-center justify-center gap-8 bg-slate-400 p-6">
+	<form
+		on:submit|preventDefault={() => {}}
+		class="grid grid-cols-2 gap-4 rounded-lg bg-white p-6 shadow-md"
+	>
+		<label for="query">Query</label>
+		<input
+			type="text"
+			name="query"
+			id="query"
+			bind:value={$urlState.query}
+			placeholder="Search..."
+		/>
+		<label for="bool">Boolean</label>
+		<input type="checkbox" name="bool" id="bool" bind:checked={$urlState.bool} />
+		<label for="slider">Slider</label>
+		<input type="range" name="slider" id="slider" min="0" max="100" bind:value={$urlState.slider} />
+	</form>
+</div>
